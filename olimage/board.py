fadaed49b14c411f62f6351dc5d7102cedf67c69
Stefan Mavrodiev <stefan@olimex.com>
2019-09-27 14:05:03 +0300

    Rework board configurations

--- olimage/board.py
+++ olimage/board.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019 Olimex Ltd.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import os
 
 import yaml
@@ -7,27 +32,40 @@ import olimage.environment as environment
 
 class Board(object):
     def __init__(self, target):
+        """
+        Default constructor. Scan <configs>/boards for target configuration.
 
+        :param target: name of the board, e.g. A64-OLinuXino-1G
+        """
+
+        self._name = None
         self._config = None
+        self._arch = None
 
         path = os.path.join(environment.paths['configs'], 'boards')
         for (_, _, files) in os.walk(path):
             for file in files:
                 with open(os.path.join(path, file), 'r') as f:
-                    cfg = yaml.full_load(f.read())
+                    config = yaml.full_load(f.read())
                 try:
-                    for t in cfg['boards']:
-                        if target.lower() in t.lower():
-                            self._config = cfg
-                            self._name = t
-                        break
+                    for key, value in config['boards'].items():
+                        if target.lower() == key.lower():
+                            self._config = value
+                            self._name = key
+                            self._arch = config['arch']
+                            break
                 except KeyError:
                     continue
 
-        if self._config is None:
+        if self._name is None:
             raise Exception("Target \'{}\' not found in configuration files!".format(target))
 
     def __str__(self):
+        """
+        Return board name
+
+        :return: string with board name
+        """
         return self._name
 
     @property
@@ -37,7 +75,7 @@ class Board(object):
 
         :return: target arch
         """
-        return self._config['arch']
+        return self._arch
 
     @property
     def packages(self):
@@ -49,7 +87,12 @@ class Board(object):
         return self._config['packages']
 
     @property
-    def hostname(self):
-        return self._config['hostname']
+    def id(self):
+        """
+        Get target board ID
+
+        :return: int id
+        """
+        return self._config['id']
 
 
