ceecb7b03e8df60e96c15a6f26b015f9356b47ce
Stefan Mavrodiev <stefan@olimex.com>
2019-11-12 13:11:32 +0200

    Improve dynamic mapping

--- olimage/core/setup/setup.py
+++ olimage/core/setup/setup.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 import olimage.environment as env
 
 from .console import Console
@@ -11,29 +13,35 @@ from .user import User
 
 class SetupMeta(type):
     def __getattribute__(self, item):
-        if item == 'console':
-            return env.obj_graph.provide(Console)
-        elif item == 'fstab':
-            return env.obj_graph.provide(FSTab)
-        elif item == 'getty':
-            return env.obj_graph.provide(Getty)
-        elif item == 'hostname':
-            return env.obj_graph.provide(Hostname)
-        elif item == 'locales':
-            return env.obj_graph.provide(Locales)
-        elif item == 'timezone':
-            return env.obj_graph.provide(Timezone)
-        elif item == 'user':
-            return env.obj_graph.provide(User)
-        else:
+
+        if env.obj_graph is None:
             return type.__getattribute__(self, item)
+
+        mapping = {
+            'console': env.obj_graph.provide(Console),
+            'fstab': env.obj_graph.provide(FSTab),
+            'getty': env.obj_graph.provide(Getty),
+            'hostname': env.obj_graph.provide(Hostname),
+            'locales': env.obj_graph.provide(Locales),
+            'timezone': env.obj_graph.provide(Timezone),
+            'user': env.obj_graph.provide(User),
+        }
+
+        if item in mapping:
+            return mapping[item]
+
+        return type.__getattribute__(self, item)
 
 
 class Setup(object, metaclass=SetupMeta):
-    console = None
-    fstab = None
-    getty = None
-    hostname = None
-    locales = None
-    timezone = None
-    user = None
+    console: Console
+    fstab: FSTab
+    getty: Getty
+    hostname: Hostname
+    locales: Locales
+    timezone: Timezone
+    user: User
+
+    @staticmethod
+    def all():
+        print("adadadasd")
