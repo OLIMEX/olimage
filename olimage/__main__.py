ebc172e5427514b9a6301e18aed9bb273f394723
Stefan Mavrodiev <stefan.mavrodiev@gmail.com>
2020-02-18 22:58:05 +0200

    Remove unused output directory

--- olimage/__main__.py
+++ olimage/__main__.py
@@ -73,7 +73,7 @@ def prepare_tree():
     if not os.path.exists(workdir):
         os.mkdir(workdir)
 
-    for directory in ['dl', 'build', 'rootfs', 'images']:
+    for directory in ['build', 'rootfs', 'images']:
         path = os.path.join(workdir, directory)
 
         # Create directory
