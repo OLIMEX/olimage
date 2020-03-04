efc300f84232139ea13592e7f5a2e21dd296bae1
Stefan Mavrodiev <stefan@olimex.com>
2020-03-04 15:51:31 +0200

    Do not run update-command-not-found on debian

--- olimage/core/setup/command_not_found.py
+++ olimage/core/setup/command_not_found.py
@@ -1,3 +1,5 @@
+import olimage.environment as env
+
 from olimage.core.io import Console
 from olimage.core.utils import Utils
 
@@ -11,7 +13,12 @@ class SetupCommandNotFound(SetupAbstract):
         with Console("Installing packages"):
             Utils.shell.chroot('apt-get install -y {}'.format(' '.join(self.packages)))
 
-        # Configure
-        with Console("Updating database"):
-            Utils.shell.chroot('update-command-not-found')
-            # Utils.shell.chroot('apt-file update')
+        """
+        On debian there is no command-not-found-data package. Thus the database
+        must be updated.
+        
+        Note: This must be run before apt-get clean
+        """
+        if str(env.objects['distribution']) == 'debian':
+            with Console("Updating database"):
+                Utils.shell.chroot('update-command-not-found')
