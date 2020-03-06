53a178a80d571680172dd8d20dfddf08b5049f48
hehopmajieh <hehopmajieh@debian.bg>
2020-03-06 17:28:59 +0200

    Add A13 support

--- olimage/core/bootloaders/allwinner/__init__.py
+++ olimage/core/bootloaders/allwinner/__init__.py
@@ -1,6 +1,7 @@
 from .a20 import BootloaderA20
+from .a13 import BootloaderA13
 from .a64 import BootloaderA64
 
 __all__ = [
-    'BootloaderA20', 'BootloaderA64'
+    'BootloaderA20', 'BootloaderA13', 'BootloaderA64'
 ]
