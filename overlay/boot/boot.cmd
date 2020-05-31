61d3206cee9a14f7ea29138ddd8b39d68714e359
Stefan Saraev <stefan@saraev.ca>
2020-05-31 22:38:36 +0300

    boot.scr: cleanup and cosmetics

--- overlay/boot/boot.cmd
+++ overlay/boot/boot.cmd
@@ -15,20 +15,21 @@ itest.b *0x10028 == 0x03 && echo "U-boot loaded from SPI"
 echo "Boot script loaded from ${devtype}"
 {% endif %}
 
+if test "${devtype}" = "scsi"; then
+    boot_dev_type=scsi
+    boot_dev_num="${scsi_bootdev}"
+else
+    boot_dev_type="mmc"
+    boot_dev_num="${mmc_bootdev}"
+fi
+
 # Load uEnv.txt
 for prefix in ${boot_prefixes}; do
     echo "Checking for ${prefix}{{ uenv.file }}..."
-    if test -e mmc ${mmc_bootdev}:{{ partitions.boot }} ${prefix}{{ uenv.file }}; then
-        load mmc ${mmc_bootdev}:1 {{ uenv.load }} ${prefix}{{ uenv.file }}
+    if test -e ${boot_dev_type} ${boot_dev_num}:{{ partitions.boot }} ${prefix}{{ uenv.file }}; then
+        load ${boot_dev_type} ${boot_dev_num}:{{ partitions.boot }} {{ uenv.load }} ${prefix}{{ uenv.file }}
         echo "Loaded environment from ${prefix}{{ uenv.file }}"
         env import -t {{ uenv.load }} ${filesize}
-    fi
-    if test "${devtype}" = "scsi"; then
-        if test -e scsi ${scsi_bootdev}:{{ partitions.boot }} ${prefix}{{ uenv.file }}; then
-            load scsi ${scsi_bootdev}:1 {{ uenv.load }} ${prefix}{{ uenv.file }}
-            echo "Loaded environment from ${prefix}{{ uenv.file }}"
-            env import -t {{ uenv.load }} ${filesize}
-        fi
     fi
 done
 
@@ -43,25 +44,14 @@ if test -n ${load_legacy} && ${load_legacy}; then
     echo "Loading legacy image format..."
 
     for prefix in ${boot_prefixes}; do
-        if test -e mmc ${mmc_bootdev}:{{ partitions.boot }} ${prefix}Image; then
-            load mmc ${mmc_bootdev}:{{ partitions.boot }} ${kernel_addr_r} ${prefix}Image
-            load mmc ${mmc_bootdev}:{{ partitions.boot }} ${fdt_addr_r} ${fdtfile}
+        if test -e ${boot_dev_type} ${boot_dev_num}:{{ partitions.boot }} ${prefix}Image; then
+            load ${boot_dev_type} ${boot_dev_num}:{{ partitions.boot }} ${kernel_addr_r} ${prefix}Image
+            load ${boot_dev_type} ${boot_dev_num}:{{ partitions.boot }} ${fdt_addr_r} ${fdtfile}
 {% if board.arch == 'arm64' %}
             booti ${kernel_addr_r} - ${fdt_addr_r}
 {% else %}
             bootz ${kernel_addr_r} - ${fdt_addr_r}
 {% endif %}
-        fi
-        if test "${devtype}" = "scsi"; then
-            if test -e scsi ${scsi_bootdev}:{{ partitions.boot }} ${prefix}Image; then
-                load scsi ${scsi_bootdev}:{{ partitions.boot }} ${kernel_addr_r} ${prefix}Image
-                load scsi ${scsi_bootdev}:{{ partitions.boot }} ${fdt_addr_r} ${fdtfile}
-{% if board.arch == 'arm64' %}
-                booti ${kernel_addr_r} - ${fdt_addr_r}
-{% else %}
-                bootz ${kernel_addr_r} - ${fdt_addr_r}
-{% endif %}
-            fi
         fi
     done
 fi
@@ -69,22 +59,12 @@ fi
 # Load kernel.itb
 echo "Loading FIT image..."
 for prefix in ${boot_prefixes}; do
-    if test -e mmc ${mmc_bootdev}:{{ partitions.boot }} ${prefix}{{ fit.file }}; then
-        load mmc ${mmc_bootdev}:{{ partitions.boot }} {{ fit.load }} ${prefix}{{ fit.file }}
+    if test -e ${boot_dev_type} ${boot_dev_num}:{{ partitions.boot }} ${prefix}{{ fit.file }}; then
+        load ${boot_dev_type} ${boot_dev_num}:{{ partitions.boot }} {{ fit.load }} ${prefix}{{ fit.file }}
         if test -n ${boot_config}; then
             bootm {{ fit.load }}#${boot_config}
         else
             bootm {{ fit.load }}
-        fi
-    fi
-    if test "${devtype}" = "scsi"; then
-        if test -e scsi ${scsi_bootdev}:{{ partitions.boot }} ${prefix}{{ fit.file }}; then
-            load scsi ${scsi_bootdev}:{{ partitions.boot }} {{ fit.load }} ${prefix}{{ fit.file }}
-            if test -n ${boot_config}; then
-                bootm {{ fit.load }}#${boot_config}
-            else
-                bootm {{ fit.load }}
-            fi
         fi
     fi
 done
