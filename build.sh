ff2b83d94e2fde5f49c5d5e615f44fd1f9e2706f
Stefan Saraev <stefan@saraev.ca>
2020-10-16 10:50:22 +0300

    meh.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# because people have no common sense
+apt-get install qemu-user-static
+
 bash run.sh -v image A64-OLinuXino buster minimal    A64-OLinuXino-buster-minimal-$(date +%Y%m%d-%H%M%S).img
 bash run.sh -v image A64-OLinuXino buster base       A64-OLinuXino-buster-base-$(date +%Y%m%d-%H%M%S).img
 bash run.sh -v image A64-OLinuXino bionic minimal    A64-OLinuXino-bionic-minimal-$(date +%Y%m%d-%H%M%S).img
