fb9f29949e7b7885c35ffca1aaa9e3bbb2823ef1
Stefan Saraev <stefan@saraev.ca>
2020-11-24 12:29:08 +0200

    extra: set journald opts

--- olimage/core/setup/extra.py
+++ olimage/core/setup/extra.py
@@ -13,3 +13,6 @@ class SetupExtra(SetupAbstract):
         Utils.install('/etc/modprobe.d/rtl8192cu.conf')
         Utils.install('/etc/modprobe.d/rtl8723bs.conf')
         Utils.shell.run('sed -i "s/^#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/g" {}/etc/systemd/system.conf'.format(env.paths['build']))
+        Utils.shell.run('sed -i "s/^#Storage=.*/Storage=volatile/g" {}/etc/systemd/journald.conf'.format(env.paths['build']))
+        Utils.shell.run('sed -i "s/^#SystemMaxUse=.*/SystemMaxUse=64M/g" {}/etc/systemd/journald.conf'.format(env.paths['build']))
+        Utils.shell.run('sed -i "s/^#RuntimeMaxUse=.*/RuntimeMaxUse=64M/g" {}/etc/systemd/journald.conf'.format(env.paths['build']))
