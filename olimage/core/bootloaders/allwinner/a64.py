9a01e618659fda196a9c374145961e2009ea4ed0
Stefan Saraev <stefan@saraev.ca>
2021-01-18 19:03:27 +0200

    boot: a64: use u-boot-sunxi-with-spl.bin

--- olimage/core/bootloaders/allwinner/a64.py
+++ olimage/core/bootloaders/allwinner/a64.py
@@ -16,30 +16,9 @@ class BootloaderA64(BootloaderAbstract):
         return 'sun50i-a64'
 
     def install(self, output: str):
-
-        source = env.paths['build'] + '/usr/lib/u-boot-olinuxino/a64-olinuxino'
-
-        # Write SPL image
-        with Console("Writing \'sunxi-spl.bin\'"):
+        with Console("Writing \'u-boot-sunxi-with-spl.bin\'"):
             Utils.shell.run(
-                'dd if={}/sunxi-spl.bin of={} bs=1k seek=8 conv=sync,fsync,notrunc'.format(source, output))
-
-        # Generate U-Boot FIT image
-        with Console("Generating \'u-boot.itb\'"):
-            temp = Utils.shell.run('mktemp -d').decode().strip()
-            Utils.shell.run(
-                'cp -vf {build}/usr/lib/arm-trusted-firmware-olinuxino/sun50i_a64/bl31.bin '
-                '{source}/sun50i-a64-olinuxino-emmc.dtb '
-                '{source}/sun50i-a64-olinuxino.dtb '
-                '{source}/u-boot-nodtb.bin '
-                '{source}/u-boot.bin '
-                '{build}/usr/bin/mksunxi_fit_atf '
-                '{temp}/'.format(build=env.paths['build'], source=source, temp=temp))
-            Utils.shell.run(
-                'cd {} && '
-                'bash mksunxi_fit_atf *.dtb > u-boot.its && '
-                'mkimage -f u-boot.its u-boot.itb'.format(temp), shell=True)
-
-        with Console("Writing \'u-boot.itb\'"):
-            Utils.shell.run('dd if={}/u-boot.itb of={} conv=notrunc,fsync bs=1k seek=40'.format(temp, output))
-            Utils.shell.run('rm -rvf {}'.format(temp))
+                'dd if={} of={} bs=1k seek=8 conv=sync,fsync,notrunc'.format(
+                    env.paths['build'] + "/usr/lib/u-boot-olinuxino/a64-olinuxino/u-boot-sunxi-with-spl.bin",
+                    output
+                ))
