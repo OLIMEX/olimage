733b4b3c117c47851954737c187575f6f790f629
Stefan Saraev <stefan@saraev.ca>
2021-01-18 19:05:39 +0200

    CI: remove bionic

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ apt-get install qemu-user-static
 
 bash run.sh -v image A64-OLinuXino buster minimal    A64-OLinuXino-buster-minimal-$(date +%Y%m%d-%H%M%S).img
 bash run.sh -v image A64-OLinuXino buster base       A64-OLinuXino-buster-base-$(date +%Y%m%d-%H%M%S).img
-bash run.sh -v image A64-OLinuXino bionic minimal    A64-OLinuXino-bionic-minimal-$(date +%Y%m%d-%H%M%S).img
-bash run.sh -v image A64-OLinuXino bionic base       A64-OLinuXino-bionic-base-$(date +%Y%m%d-%H%M%S).img
+bash run.sh -v image A64-OLinuXino focal minimal     A64-OLinuXino-focal-minimal-$(date +%Y%m%d-%H%M%S).img
+bash run.sh -v image A64-OLinuXino focal base        A64-OLinuXino-focal-base-$(date +%Y%m%d-%H%M%S).img
 
 bash run.sh -v image A20-OLinuXino buster minimal    A20-OLinuXino-buster-minimal-$(date +%Y%m%d-%H%M%S).img
 bash run.sh -v image A20-OLinuXino buster base       A20-OLinuXino-buster-base-$(date +%Y%m%d-%H%M%S).img
-bash run.sh -v image A20-OLinuXino bionic minimal    A20-OLinuXino-bionic-minimal-$(date +%Y%m%d-%H%M%S).img
-bash run.sh -v image A20-OLinuXino bionic base       A20-OLinuXino-bionic-base-$(date +%Y%m%d-%H%M%S).img
+bash run.sh -v image A20-OLinuXino focal minimal     A20-OLinuXino-focal-minimal-$(date +%Y%m%d-%H%M%S).img
+bash run.sh -v image A20-OLinuXino focal base        A20-OLinuXino-focal-base-$(date +%Y%m%d-%H%M%S).img
