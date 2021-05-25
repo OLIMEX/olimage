a1ee56f273cebbddfbb8d92ae7f754f68e30a80b
Stefan Saraev <stefan@saraev.ca>
2021-05-25 10:40:10 +0300

    extra: adjust journald.conf (runtime file/max 8/16)

--- olimage/core/setup/extra.py
+++ olimage/core/setup/extra.py
@@ -15,4 +15,5 @@ class SetupExtra(SetupAbstract):
         Utils.shell.run('sed -i "s/^#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/g" {}/etc/systemd/system.conf'.format(env.paths['build']))
         Utils.shell.run('sed -i "s/^#Storage=.*/Storage=volatile/g" {}/etc/systemd/journald.conf'.format(env.paths['build']))
         Utils.shell.run('sed -i "s/^#SystemMaxUse=.*/SystemMaxUse=64M/g" {}/etc/systemd/journald.conf'.format(env.paths['build']))
-        Utils.shell.run('sed -i "s/^#RuntimeMaxUse=.*/RuntimeMaxUse=64M/g" {}/etc/systemd/journald.conf'.format(env.paths['build']))
+        Utils.shell.run('sed -i "s/^#RuntimeMaxFileSize=.*/RuntimeMaxFileSize=8M/g" {}/etc/systemd/journald.conf'.format(env.paths['build']))
+        Utils.shell.run('sed -i "s/^#RuntimeMaxUse=.*/RuntimeMaxUse=16M/g" {}/etc/systemd/journald.conf'.format(env.paths['build']))
